5ad1ed183a22c1c147245135017e0a01bf1cfba2
Mariya Goliyad <goliyad@br012.ib.bridges2.psc.edu>
2026-03-02 21:08:19 -0500

    removed campaign_manager and kept resource manager only

--- src/campaign/__init__.py
+++ src/campaign/__init__.py
@@ -1,7 +1,5 @@
 """Campaign management for multi-workflow orchestration."""
 
-from .base_workflow import BaseWorkflow, TaskSpec
-from .campaign_manager import CampaignManager
 from .resource_manager import ResourceManager
 
-__all__ = ["CampaignManager", "BaseWorkflow", "TaskSpec", "ResourceManager"]
+__all__ = ["ResourceManager"]
